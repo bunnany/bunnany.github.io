74306815b48dce6c1c2cb23aca415224f90f0567
bunnany <45374304+bunnany@users.noreply.github.com>
2024-03-11 17:06:38 +1300

    sushi to saucy commit

---
+++ marketday2023/saucy_sliders/script.js
@@ -0,0 +1,27 @@
+// Nav Icon //
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+// Faqs //
+var acc = document.getElementsByClassName("accordian");
+var i;
+
+for(i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if(panel.style.display === "block"){
+            panel.style.display = "none";
+        }else{
+            panel.style.display = "block";
+        }  
+    });
+}
+
